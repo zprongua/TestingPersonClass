c9a2c9ba45785efc759412490c0d6bd326348d24
KrYounger <35814586+xt0fer@users.noreply.github.com>
2022-10-19 15:03:14 -0400

    Initial commit

---
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -0,0 +1,98 @@
+package com.zipcodewilmington.person;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by leon on 2/12/18.
+ */
+public class TestPerson {
+    @Test
+    public void testDefaultConstructor() {
+        // Given
+        String expectedName = "";
+        Integer expectedAge = Integer.MAX_VALUE;
+
+        // When
+        Person person = new Person();
+
+        // Then
+        String actualName = person.getName();
+        Integer actualAge = person.getAge();
+
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedAge, actualAge);
+    }
+
+    @Test
+    public void testConstructorWithName() {
+        // Given
+        String expected = "Leon";
+
+        // When
+        Person person = new Person(expected);
+
+        // Then
+        String actual = person.getName();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConstructorWithAge() {
+        // Given
+        Integer expected = 5;
+
+        // When
+        Person person = new Person(expected);
+
+        // Then
+        Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testConstructorWithNameAndAge() {
+        // Given
+        Integer expectedAge = 5;
+        String expectedName = "Leon";
+
+        // When
+        Person person = new Person(expectedName, expectedAge);
+
+        // Then
+        Integer actualAge = person.getAge();
+        String actualName = person.getName();
+
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedName, actualName);
+    }
+
+    @Test
+    public void testSetName() {
+        // Given
+        Person person = new Person();
+        String expected = "Leon";
+
+        // When
+        person.setName(expected);
+        String actual = person.getName();
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetAge() {
+        // Given
+        Person person = new Person();
+        Integer expected = 5;
+
+        // When
+        person.setAge(expected);
+
+        // Then
+        Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+}
