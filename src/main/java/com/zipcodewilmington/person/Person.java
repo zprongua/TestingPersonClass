d876922b33c212ab9f6a1659987aee857f4ab3a5
zprongua <115650795+zprongua@users.noreply.github.com>
2022-11-01 17:51:56 -0400

    Finished base Person.java, all given tests pass

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,32 +4,44 @@ package com.zipcodewilmington.person;
  * Created by leon on 2/12/18.
  */
 public class Person {
-    private String name;
-    private int age;
+    private String name = "";
+    private int age = Integer.MAX_VALUE;
 
     public Person() {
     }
 
-    public Person(int age) {
+    //can't use same variable as class field, personally would rather change incoming var then name inside class
+    //public Person(int age) {}
+    public Person(int newAge) {
+        age = newAge;
     }
 
-    public Person(String name) {
+    //can't use same variable as class field
+    //public Person(String name) {}
+    public Person(String newName) {
+        name = newName;
     }
 
-    public Person(String name, int age) {
+    //can't use same variable as class field
+    //public Person(String name, int age) {}
+    public Person(String newName, int newAge) {
+        name = newName;
+        age = newAge;
     }
 
-    public void setName(String name) {
-    }
+    //can't use same variable as class field
+    //public void setName(String name) {}
+    public void setName(String newName) { name = newName; }
 
-    public void setAge(int age) {
-    }
+    //can't use same variable as class field
+    //public void setAge(int age) {}
+    public void setAge(int newAge) { age = newAge; }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
 }
